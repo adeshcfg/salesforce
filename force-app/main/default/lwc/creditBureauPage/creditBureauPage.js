a46449fc1c0cd0c9454bb7679e5b536870fcdc99
adeshcfg <adesh.ikhar@canaccede.com>
2024-04-25 16:42:53 +0530

    6487: Update UI and add work assignment link

--- force-app/main/default/lwc/creditBureauPage/creditBureauPage.js
+++ force-app/main/default/lwc/creditBureauPage/creditBureauPage.js
@@ -1,24 +1,35 @@
-import { LightningElement } from 'lwc';
-import getAccounts from '@salesforce/apex/creditBureauClass.getcreditBureauClass';
+import { LightningElement, wire } from "lwc";
+import getAccounts from "@salesforce/apex/creditBureauClass.getcreditBureauClass";
+import getWorkAssignment from "@salesforce/apex/creditBureauClass.getWorkAssignment";
 
 export default class creditBureauClass extends LightningElement {
+    workAssignmentId = "";
     CreditBureauDisabled = false;
     TransUnionDisabled = false;
     EquifaxDisabled = false;
-    handleClick(event){
+
+    @wire(getWorkAssignment)
+    wiredAssignment({ error, data }) {
+        if (data) {
+            this.workAssignmentId = `/${data}`;
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleClick(event) {
         let labelName = event.target.label;
-        if(labelName == 'CreditBureau'){
-            getAccounts({ batchName: labelName })
+        if (labelName === "CreditBureau") {
+            getAccounts({ batchName: labelName });
             this.CreditBureauDisabled = true;
         }
-        if(labelName == 'TransUnion'){
-            getAccounts({ batchName: labelName })
+        if (labelName === "TransUnion") {
+            getAccounts({ batchName: labelName });
             this.TransUnionDisabled = true;
         }
-        if(labelName == 'Equifax'){
-            getAccounts({ batchName: labelName })
+        if (labelName === "Equifax") {
+            getAccounts({ batchName: labelName });
             this.EquifaxDisabled = true;
         }
-        
     }
 }
